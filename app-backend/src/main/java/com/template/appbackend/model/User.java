d420e99addb3a19c7b278d1f9ba57c769b5d910f
JostinDev <justinnydegger@hotmail.com>
2023-10-05 19:41:45 +0200

    Add Lombok to reduce boilerplate code

--- app-backend/src/main/java/com/template/appbackend/model/User.java
+++ app-backend/src/main/java/com/template/appbackend/model/User.java
@@ -3,48 +3,22 @@ package com.template.appbackend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "user")
+@Getter @Setter @NoArgsConstructor
 public class User {
 
-    @Id
-    private String email;
+    private @Id String email;
     private String name;
     private String avatar;
-
-    public User() {
-
-    }
 
     public User (String email, String name, String avatar) {
         this.email = email;
         this.name = name;
-        this.avatar = avatar;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
         this.avatar = avatar;
     }
 
